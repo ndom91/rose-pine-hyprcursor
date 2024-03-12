96b6df4eb6299d31db57efa42e943e503f1fc676
ndom91 <yo@ndo.dev>
2024-03-12 16:16:19 +0100

    fix: update github commit rev

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = pkgs.fetchFromGitHub {
     owner = "ndom91";
     repo = "rose-pine-cursor-hyprcursor";
-    rev = "f96a8e09fa7e60d5e48543e15693774dcf092fbe";
+    rev = "e4dde6d19da2d048cf4b224fe2ed1576c9c50087";
     sha256 = "sha256-h7qivaCqoJj1ZbWT86WA1gp769vgTUhDDxcJOZ1WfMU=";
   };
 
