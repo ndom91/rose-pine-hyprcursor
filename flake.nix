123020e7408d9f32c235f75b362faccccf997108
ndom91 <yo@ndo.dev>
2024-03-14 16:22:05 +0100

    fix: test flake.nxi update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default-linux";
 
     hyprlang = {
       url = "github:hyprwm/hyprlang";
@@ -12,7 +11,7 @@
   };
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, hyprlang }:
     let
       genSystems = nixpkgs.lib.genAttrs [
         "x86_64-linux"
@@ -23,25 +22,27 @@
     in
     {
       overlays.default = _: prev: {
-        rose-pine-cursor-hyprland = prev.callPackage ./nix/default.nix { };
+        rose-pine-cursor-hyprland = prev.callPackage ./nix/default.nix { nixpkgs = prev; };
       };
 
       packages = genSystems (system: self.overlays.default null pkgsFor.${system});
 
-      config = {
-        home.pointerCursor = {
-          gtk.enable = true;
-          x11.enable = true;
-          package = rose-pine-cursor-hyprcursor;
-          name = "rose-pine-cursor-hyprcursor";
-          size = 24;
-        };
-        gtk.cursorTheme = {
-          package = rose-pine-cursor-hyprcursor;
-          name = "rose-pine-cursor-hyprcursor";
-          size = 24;
-        };
-      };
+      modules = [
+        ({ lib, ... }: {
+          home.pointerCursor = {
+            gtk.enable = true;
+            x11.enable = true;
+            package = rose-pine-cursor-hyprcursor;
+            name = "rose-pine-cursor-hyprcursor";
+            size = 24;
+          };
+          gtk.cursorTheme = {
+            package = rose-pine-cursor-hyprcursor;
+            name = "rose-pine-cursor-hyprcursor";
+            size = 24;
+          };
+        })
+      ];
 
       formatter = genSystems (system: pkgsFor.${system}.nixpkgs-fmt);
     };
